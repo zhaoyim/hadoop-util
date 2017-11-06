0fb31ae0c7c8ba2a7a3c3400571627085d0a6729
chenxf <chenxuefeng1207@163.com>
2017-11-06 18:32:32 +0800

    add unittest

--- test/config_util_test.py
+++ test/config_util_test.py
@@ -8,11 +8,20 @@ from config_util import ConfigUtil
 
 
 class TestConfigUtil(unittest.TestCase):
+    """
+    test ConfigUtil's methods
+    """
 
     def test_load_config(self):
+        """
+        test ConfigUtil.load_config's exception
+        """
         self.assertRaises(IOError, ConfigUtil.load_config, "./conf/conf.conf")
 
     def test_get_options(self):
+        """
+        test if  ConfigUtil.get_options can resolve exception correct
+        """
         config_file = "../conf/properties.conf"
         config_util = ConfigUtil(config_file)
         self.assertEqual(config_util.get_options("url", "hadoop_url"),
@@ -23,6 +32,9 @@ class TestConfigUtil(unittest.TestCase):
                           config_util.get_options, "url2", "hadoop_url")
         self.assertRaises(KeyError,
                           config_util.get_options, "url2")
+
+if __name__ == "__main__":
+    unittest.main()
 
 
 
