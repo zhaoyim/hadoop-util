531f2e7fe3b0f0e9a6ef88f33e4287ff4f6d429e
chenxf <chenxuefeng1207@163.com>
2017-11-10 14:43:57 +0800

    20171110

--- logger_util.py
+++ logger_util.py
@@ -4,16 +4,21 @@ Created on  : 11/10/17
 @author: chenxf@chinaskycloud.com
 """
 import logging
+from file_operator import FileOperator
+import os
 
 
 def get_logger(refresh=False):
+    log_path = "./log"
+    log_file = os.path.join(log_path, "logs")
+    FileOperator.makesure_file_exits(log_path)
     if refresh:
         get_logger.logger = None
     if get_logger.logger:
         return get_logger.logger
     logger = logging.getLogger()
     logger.setLevel(logging.WARNING)
-    file_hadler = logging.FileHandler("./log/logs")
+    file_hadler = logging.FileHandler(log_file)
     file_hadler.setLevel(logging.WARNING)
     stream_hadler = logging.StreamHandler()
     stream_hadler.setLevel(logging.WARNING)
